96566c7e59b86615e9170ffafc3639cf5d1e423a
tosif91 <61594032+tosif91@users.noreply.github.com>
2022-02-17 13:08:54 +0530

    added maze paths solution

---
+++ recursion/Mazepaths.java
@@ -0,0 +1,45 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Mazepaths {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
+        ArrayList<String> paths = getMazePaths(0,0,2,2);
+        System.out.println(paths);
+
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        ArrayList<String> paths = new ArrayList<String>();
+        if (sr == dr && sc == dc) {
+            paths.add("");
+            return paths;
+        }
+
+        if (sr > dr || sc > dc) {
+            return paths;
+        }
+
+        ArrayList<String> hPaths = getMazePaths(sr, sc + 1, dr, dc);
+
+        ArrayList<String> vPaths = getMazePaths(sr + 1, sc, dr, dc);
+
+        for (String val : hPaths) {
+            paths.add('h' + val);
+        }
+        for (String val : vPaths) {
+            paths.add('v' + val);
+        }
+
+        return paths;
+    }
+}
