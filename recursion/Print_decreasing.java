9b3965642bba3eb802c8bac6e2746bbe4f3296ce
tosif91 <61594032+tosif91@users.noreply.github.com>
2022-02-17 10:40:35 +0530

    added basic recurssion question

---
+++ recursion/Print_decreasing.java
@@ -0,0 +1,21 @@
+package recursion;
+import java.util.Scanner;
+
+public class Print_decreasing {
+    
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        printDecreasing(n);
+    }
+
+    public static void printDecreasing(int n){
+        if(n==0)
+            return;
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+}
+
